6e6971c6930a379698326542077dbf2689788956
YusukeKyokawa <kiyo@YusukeKyokawanoMacBook-Pro.local>
2019-08-23 14:57:25 +0900

    delete model

--- myalgo1.py
+++ myalgo1.py
@@ -136,17 +136,7 @@ if __name__ == "__main__":
     model.add(Dense(2048, activation='relu'))
     model.add(Dropout(0.5))
     model.add(Dense(num_classes, activation='softmax'))
-    # model = Sequential()
-    # model.add(Conv2D(16, (3, 3), input_shape=(img_size, img_size , 1), activation='relu'))
-    # model.add(Conv2D(32, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Conv2D(64, (3, 3), activation='relu'))
-    # model.add(MaxPooling2D(pool_size=(2, 2)))
-    # model.add(Dropout(0.25))
-    # model.add(Flatten())
-    # model.add(Dense(128, activation='relu'))
-    # model.add(Dropout(0.25))
-    # model.add(Dense(num_classes, activation='softmax'))
+
 
     model.compile(loss='categorical_crossentropy', optimizer=Adam(), metrics=['accuracy'])
 
